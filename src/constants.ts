2531482406c2ac92475aba6a1aec9cddbcb8d5f1
Adam Lund <lundmn@gmail.com>
2024-04-14 18:59:43 -0500

    Refactor region map

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,10 @@
-// Considered parameterizing this and would ask customer to allow changing page_size
+/** Considered parameterizing this and would suggest to customer allowing variable page_size */
 export const DEFAULT_PAGE_SIZE = 15;
+
+/** Non-exhaustive, may move to store in db */
+export const mapRegionToCountry = {
+  us: { value: 'United States' },
+  uk: { value: 'United Kingdom' },
+  ca: { value: 'Canada' },
+  au: { value: 'Australia' },
+};
