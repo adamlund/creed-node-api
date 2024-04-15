746a76cda8e354bc1174e219112664f148493f95
Adam Lund <lundmn@gmail.com>
2024-04-15 08:30:54 -0500

    Add podcastss route test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app/app.module';
+import { KnexService } from '../src/db/db.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,25 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/healthz (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/healthz')
-      .expect(200)
-      .expect({ dbConnection: true });
+  afterAll(async () => {
+    await app.get(KnexService).closeKnex();
+    await app.close();
+  });
+
+  it('/healthz (GET)', async () => {
+    const result = await request(app.getHttpServer()).get('/healthz');
+    expect(result.statusCode).toEqual(200);
+    expect(result.body.dbConnection).toEqual(true);
+  });
+
+  it('/podcasts/best_podcasts (GET)', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/podcasts/best_podcasts')
+      .query({ genre_id: 140, page: 1, page_size: 5 });
+
+    expect(result.statusCode).toEqual(200);
+    expect(result.body).toHaveProperty('podcasts');
+    expect(result.body.podcasts).toBeInstanceOf(Array);
+    expect(result.body.podcasts).toHaveLength(5);
   });
 });
