5f7110e5362d3642b373de03480131b5f996970e
Adam Lund <lundmn@gmail.com>
2024-04-14 19:03:13 -0500

    Refactored region map

--- src/podcasts/filter/filter.query.ts
+++ src/podcasts/filter/filter.query.ts
@@ -1,20 +1,13 @@
 import { QueryBuilderType } from 'objection';
 import { PodcastModel } from 'src/db/models/Podcast.model';
-import { GetPodcastsDto } from "../dto/podcasts.dto";
-
-/** Variable used as an example, non-exhaustive */
-const countryMap = {
-  'us' : { value: 'United States'},
-  'uk' : { value: 'United Kingdom'},
-  'ca' : { value: 'Canada'},
-  'au' : { value: 'Australia'},
-};
+import { GetPodcastsDto } from '../dto/podcasts.dto';
+import { mapRegionToCountry } from '../../constants';
 
 /**
- * Build Knex/objection query for supported query params from Dto.
+ * Build Knex/objection ORM for supported query params from Dto.
  * QueryBuilder modifies passed object in place and does note return a value.
  * @param queryBuilder PodcastModel Objection QueryBuilder
- * @param query 
+ * @param query
  */
 export function applyDomainFilters(
   queryBuilder: QueryBuilderType<PodcastModel>,
@@ -23,8 +16,8 @@ export function applyDomainFilters(
   if (query?.safe_mode) {
     queryBuilder.where('explicit_content', query.safe_mode);
   }
-  if (query?.region && Object.keys(countryMap).includes(query.region)) {
-    queryBuilder.where('country', countryMap[query.region].value);
+  if (query?.region && Object.keys(mapRegionToCountry).includes(query.region)) {
+    queryBuilder.where('country', mapRegionToCountry[query.region].value);
   }
   if (query?.genre_id) {
     queryBuilder.whereRaw('JSON_CONTAINS(genre_ids, CAST(? AS JSON))', [
