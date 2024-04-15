aa0bf4b0abb03f5e33abb08be23b5279bd4cc46f
Adam Lund <lundmn@gmail.com>
2024-04-14 19:08:08 -0500

    Improve paging to add page_size

--- src/podcasts/podcasts.service.ts
+++ src/podcasts/podcasts.service.ts
@@ -25,14 +25,16 @@ export class PodcastsService {
 
   /**
    * Retrieve Genre from DB by numeric id.
-   * @param genreId 
-   * @returns 
+   * @param genreId
+   * @returns
    */
   async fetchGenre(genreId: number): Promise<GenreDto> {
     // In a production system I would cache then genre array using Redis or Memcached as it rarely changes
     const genre = await this.GenreClass.query().findById(genreId);
     if (!genre) {
-      throw new BadRequestException(`Unsupported genre_id provided, genre_id=${genreId}`);
+      throw new BadRequestException(
+        `Unsupported genre_id provided, genre_id=${genreId}`,
+      );
     }
     return genre;
   }
@@ -42,32 +44,43 @@ export class PodcastsService {
    * @param query see GetPodcastsDto for params
    * @returns Podcast search result with genre metadata and pagination
    */
-  async fetchPodcasts(query: GetPodcastsDto): Promise<Partial<PodcastResultDto>> {
-    const genre = query.genre_id ? await this.fetchGenre(query.genre_id) : this.defaultGenre();
+  async fetchPodcasts(
+    query: GetPodcastsDto,
+  ): Promise<Partial<PodcastResultDto>> {
+    const genre = query.genre_id
+      ? await this.fetchGenre(query.genre_id)
+      : this.defaultGenre();
 
-    // sanitize page number input
+    // sanitize page inputs
     const pageNumber = getPageNumber(query.page);
-    
+    const pageSize = query.page_size || DEFAULT_PAGE_SIZE;
+
     // building query object
     const queryBuilder = this.PodcastClass.query();
     applyDomainFilters(queryBuilder, query);
 
-    let queryResult = await queryBuilder.page(pageNumber - 1, DEFAULT_PAGE_SIZE);
+    let queryResult = await queryBuilder.page(
+      pageNumber - 1,
+      pageSize,
+    );
     let actualPageNumber = pageNumber;
 
-    const totalPages = Math.ceil(queryResult.total / DEFAULT_PAGE_SIZE);
+    const totalPages = Math.ceil(queryResult.total / pageSize);
 
     // If paged beyond the maximum, provide the data from last page rather than zero results
     if (pageNumber > totalPages) {
       actualPageNumber = Math.max(totalPages, 1);
-      queryResult = await queryBuilder.page(actualPageNumber - 1, DEFAULT_PAGE_SIZE);
+      queryResult = await queryBuilder.page(
+        actualPageNumber - 1,
+        pageSize,
+      );
     }
 
     const paging = pagingResponse(
       queryResult.total,
       queryResult.results.length,
       actualPageNumber,
-      DEFAULT_PAGE_SIZE
+      pageSize,
     );
 
     const responseResult = {
